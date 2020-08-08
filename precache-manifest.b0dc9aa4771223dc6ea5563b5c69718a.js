22e83405d810ecc778064d22bf4574f2cfe638a7
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-08 23:41:32 +0530

    Updates

---
+++ precache-manifest.b0dc9aa4771223dc6ea5563b5c69718a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b62c782f05bb64f71ed284c37e972b5",
+    "url": "/mern_auth_front/index.html"
+  },
+  {
+    "revision": "956c232d41135aa073df",
+    "url": "/mern_auth_front/static/css/main.25a36dcd.chunk.css"
+  },
+  {
+    "revision": "b591d46a2888508605af",
+    "url": "/mern_auth_front/static/js/2.49e03d1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mern_auth_front/static/js/2.49e03d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956c232d41135aa073df",
+    "url": "/mern_auth_front/static/js/main.15e6aa44.chunk.js"
+  },
+  {
+    "revision": "374188d7e136c285ed9b",
+    "url": "/mern_auth_front/static/js/runtime-main.b1bd926e.js"
+  }
+]);
